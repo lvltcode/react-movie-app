9e3cdc27fb99b5ffbc8d53bb7efd42617183a79d
michellenguyen <ngan.nguyen1186@gmail.com>
2019-03-26 11:10:40 +0700

    adding Modal for Trailer

--- src/MoviesList.js
+++ src/MoviesList.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 import Movie from "./Movie";
+import { Row } from 'reactstrap';
+import ReactModal from 'react-modal';
 
 export default class MoviesList extends Component {
   render() {
     if (this.props.movies.length > 0) {
       return (
-        <div>
+        <Row className="content">
           {this.props.movies.map(movie => (
             <Movie {...movie} />
           ))}
-        </div>
+        </Row>
       );
     } else {
       return <h1 className="m-3">No Movies</h1>;
     }
   }
 }
+
